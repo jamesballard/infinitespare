1443c67cd13748841f3a1ab8fdea4afacef70bef
Robert Egglestone <r.egglestone@auckland.ac.nz>
2013-01-28 15:51:52 +1300

    Fix grant option

--- install.sh
+++ install.sh
@@ -155,8 +155,7 @@ class DATABASE_CONFIG {
         );
 EOF
 
-		echo mysql -BNe "grant all privileges on $dbname.* to $dbuser@'% identified by '$dbpass';" $dbname
-		mysql -BNe "grant all privileges on $dbname.* to $dbuser@'% identified by '$dbpass';" $dbname
+		mysql -BNe "grant all privileges on $dbname.* to $dbuser@'%' identified by '$dbpass';" $dbname
 	fi
 
 	# if database is new, populate it
